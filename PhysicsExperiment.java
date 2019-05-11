7bedf35a37a12cd1d09d1c64563bbe860b86ca45
skhan176 <43834676+skhan176@users.noreply.github.com>
2019-05-10 21:14:43 -0400

    Updated PhysicsExperiment.java

--- PhysicsExperiment.java
+++ PhysicsExperiment.java
@@ -1,7 +1,8 @@
+
 /**
  * Physics Experiment
- * Author: Your Name and Carolyn Yao
- * Does this compile or finish running within 5 seconds? Y/N
+ * Author: sakib khan and  Carolyn Yao
+ * Does this compile or finish running within 5 seconds? Y
  */
 
 /**
@@ -11,155 +12,153 @@
  * solution.
  *
  * You will only be graded on code you add to the scheduleExperiments method.
- * Do not mess with the existing formatting and identation.
+ * Do not mess with the existing formatting and indentation.
  * You don't need to use the helper methods, but if they come in handy setting
  * up a custom test case, feel free to use them.
  */
-public class PhysicsExperiment {
 
-  /**
-   * The actual greedy scheduler you will be implementing!
-   * @param numStudents The number of students who can participate, m
-   * @param numSteps The number of steps in the experiment, n
-   * @param signUpTable An easy lookup tool, signUpTable[x][Y] = student X signed up or did not sign up for step Y.
-   *      Example:
-          signUpTable[1][3] = 1 if Student 1 signed up for Step 3
-          signUpTable[1][3] = 0 if Student 1 didn't sign up for Step 3
-   * @return scheduleTable: a table similar to the signUpTable where scheduleTable[X][Y] = 1 means
-   *     student X is assigned to step Y in an optimal schedule
-   */
-  public int[][] scheduleExperiments(
-    int numStudents,
-    int numSteps,
-    int[][] signUpTable
-  ) {
-    // Your scheduleTable is initialized as all 0's so far. Your code will put 1's
-    // in the table in the right places based on the return description
-    int[][] scheduleTable = new int[numStudents + 1][numSteps + 1];
 
-    // Your code goes here
 
-    return scheduleTable;
-  }
 
-  /**
-   * Makes the convenient lookup table based on the steps each student says they can do
-   * @param numSteps the number of steps in the experiment
-   * @param studentSignUps student sign ups ex: {{1, 2, 4}, {3, 5}, {6, 7}}
-   * @return a lookup table so if we want to know if student x can do step y,
-      lookupTable[x][y] = 1 if student x can do step y
-      lookupTable[x][y] = 0 if student x cannot do step y
-   */
-  public int[][] makeSignUpLookup(int numSteps, int[][] studentSignUps) {
-    int numStudents = studentSignUps.length;
-    int[][] lookupTable = new int[numStudents+1][numSteps + 1];
-    for (int student = 1; student <= numStudents; student++) {
-      int[] signedUpSteps = studentSignUps[student-1];
-      for (int i = 0; i < signedUpSteps.length; i++) {
-        lookupTable[student][signedUpSteps[i]] = 1;
-      }
-    }
-    return lookupTable;
-  }
 
-  /**
-   * Prints the optimal schedule by listing which steps each student will do
-   * Example output is Student 1: 1, 3, 4
-   * @param schedule The table of 0's and 1's of the optimal schedule, where
-   *   schedule[x][y] means whether in the optimal schedule student x is doing step y
-   */
-  public void printResults(int[][] schedule) {
-    for (int student = 1; student < schedule.length; student++) {
-      int[] curStudentSchedule = schedule[student];
-      System.out.print("Student " + student + ": ");
-      for (int step = 1; step < curStudentSchedule.length; step++) {
-        if (curStudentSchedule[step] == 1) {
-          System.out.print(step + " ");
-        }
-      }
-      System.out.println("");
-    }
-  }
+public class PhysicsExperiment{
 
-  /**
-   * This validates the input data about the experiment step sign-ups.
-   * @param numStudents the number of students
-   * @param numSteps the number of steps
-   * @param signUps the data given about which steps each student can do
-   * @return true or false whether the input sign-ups match the given number of
-   *    students and steps, and whether all the steps are guaranteed at least
-   *    one student.
-   */
-  public boolean inputsValid(int numStudents, int numSteps, int signUps[][]) {
-    int studentSignUps = signUps.length;
+	  public int[][] scheduleExperiments(
+	    int numStudents,
+	    int numSteps,
+	    int[][] signUpTable
+	  ) {
+	    // Your scheduleTable is initialized as all 0's so far. Your code will put 1's
+	    // in the table in the right places based on the return description
+	    int[][] scheduleTable = new int[numStudents + 1][numSteps + 1];
+	
 
-    // Check if there are any students or signups
-    if (numStudents < 1 || studentSignUps < 1) {
-      System.out.println("You either did not put in any student or any signups");
-      return false;
-    }
+	    int current_step_num = 1;
+	    while(current_step_num <= numSteps)
+	    {
+	    	int max_steps = 0;
+		    int max_step_owner = 0;
+		    for(int student = 1; student <= numStudents; student++)
+		    {
+		    	int steps = 0;
+		    	while(signUpTable[student][current_step_num + steps] != 0)
+		    	{
+		    		steps ++;
+		    		if(current_step_num + steps > numSteps)
+		    			break;
+		    	}
+		    	if(steps > max_steps)
+		    	{
+		    		max_steps = steps;
+		    		max_step_owner = student;
+		    	}
+		    }
+		    for(int k = 0; k < max_steps; k++)
+		    {
+		    	scheduleTable[max_step_owner][current_step_num + k] = 1;
+		    }
+		    current_step_num += max_steps;
+	    }
+	    
+	
 
-    // Check if the number of students and sign-up rows matches
-    if (numStudents != studentSignUps) {
-      System.out.println("You input " + numStudents + " students but your signup suggests " + signUps.length);
-      return false;
-    }
+	    return scheduleTable;
+	  }
+	
 
-    // Check that all steps are guaranteed in the signups
-    int[] stepsGuaranteed = new int[numSteps + 1];
-    for (int i = 0; i < studentSignUps; i++) {
-      for (int j = 0; j < signUps[i].length; j++) {
-        stepsGuaranteed[signUps[i][j]] = 1;
-      }
-    }
-    for (int step = 1; step <= numSteps; step++) {
-      if (stepsGuaranteed[step] != 1) {
-        System.out.println("Your signup is incomplete because not all steps are guaranteed.");
-        return false;
-      }
-    }
+	  public int[][] makeSignUpLookup(int numSteps, int[][] studentSignUps) {
+	    int numStudents = studentSignUps.length;
+	    int[][] lookupTable = new int[numStudents+1][numSteps + 1];
+	    for (int student = 1; student <= numStudents; student++) {
+	      int[] signedUpSteps = studentSignUps[student-1];
+	      for (int p = 0; p < signedUpSteps.length; p++) {
+	        lookupTable[student][signedUpSteps[p]] = 1;
+	      }
+	    }
+	    return lookupTable;
+	  }
+	
 
-    return true;
-  }
+	  public void printResults(int[][] schedule) {
+	    for (int student = 1; student < schedule.length; student++) {
+	      int[] curStudentSchedule = schedule[student];
+	      System.out.print("Student " + student + ": ");
+	      for (int step = 1; step < curStudentSchedule.length; step++) {
+	        if (curStudentSchedule[step] == 1) {
+	          System.out.print(step + " ");
+	        }
+	      }
+	      System.out.println("");
+	    }
+	  }
+	
 
-  /**
-   * This sets up the scheduling test case and calls the scheduling method.
-   * @param numStudents the number of students
-   * @param numSteps the number of steps
-   * @param signUps which steps each student can do, in order of students and steps
-   */
-  public void makeExperimentAndSchedule(int experimentNum, int numStudents, int numSteps, int[][] signUps) {
-    System.out.println("----Experiment " + experimentNum + "----");
-    if (!inputsValid(numStudents, numSteps, signUps)) {
-      System.out.println("Experiment signup info is invalid");
-      return;
-    }
-    int[][] signUpsLookup = makeSignUpLookup(numSteps, signUps);
-    int[][] schedule = scheduleExperiments(numStudents, numSteps, signUpsLookup);
-    printResults(schedule);
-    System.out.println("");
-  }
+	  public boolean inputsValid(int numStudents, int numSteps, int signUps[][]) {
+	    int studentSignUps = signUps.length;
+	
 
-  /**
-   * You can make additional test cases using the same format. In fact the helper functions
-   * I've provided will even check your test case is set up correctly. Do not touch any of
-   * of the existing lines. Just make sure to comment out or delete any of your own test cases
-   * when you submit. The three experiment test cases existing in this main method should be
-   * the only output when running this file.
-   */
-  public static void main(String args[]){
-    PhysicsExperiment pe = new PhysicsExperiment();
+	    // Check if there are any students or signups
+	    if (numStudents < 1 || studentSignUps < 1) {
+	      System.out.println("You either did not put in any student or any signups");
+	      return false;
+	    }
+	
 
-    // Experiment 1: Example 1 from README, 3 students, 6 steps:
-    int[][] signUpsExperiment1 = {{1, 2, 3, 5}, {2, 3, 4}, {1, 4, 5, 6}};
-    pe.makeExperimentAndSchedule(1, 3, 6, signUpsExperiment1);
+	    // Check if the number of students and sign-up rows matches
+	    if (numStudents != studentSignUps) {
+	      System.out.println("You input " + numStudents + " students but your signup suggests " + signUps.length);
+	      return false;
+	    }
+	
 
-    // Experiment 2: Example 2 from README, 4 students, 8 steps
-    int[][] signUpsExperiment2 = {{5, 7, 8}, {2, 3, 4, 5, 6}, {1, 5, 7, 8}, {1, 3, 4, 8}};
-    pe.makeExperimentAndSchedule(2, 4, 8, signUpsExperiment2);
+	    // Check that all steps are guaranteed in the signups
+	    int[] stepsGuaranteed = new int[numSteps + 1];
+	    for (int n = 0; n < studentSignUps; n++) {
+	      for (int m = 0; m < signUps[n].length; m++) {
+	        stepsGuaranteed[signUps[n][m]] = 1;
+	      }
+	    }
+	    for (int step = 1; step <= numSteps; step++) {
+	      if (stepsGuaranteed[step] != 1) {
+	        System.out.println("Your signup is incomplete because not all steps are guaranteed.");
+	        return false;
+	      }
+	    }
+	
 
-    // Experiment 3: Another test case, 5 students, 11 steps
-    int[][] signUpsExperiment3 = {{7, 10, 11}, {8, 9, 10}, {2, 3, 4, 5, 7}, {1, 5, 6, 7, 8}, {1, 3, 4, 8}};
-    pe.makeExperimentAndSchedule(3, 5, 11, signUpsExperiment3);
-  }
+	    return true;
+	  }
+	
+
+	  public void makeExperimentAndSchedule(int experimentNum, int numStudents, int numSteps, int[][] signUps) {
+	    System.out.println("----Experiment " + experimentNum + "----");
+	    if (!inputsValid(numStudents, numSteps, signUps)) {
+	      System.out.println("Experiment signup info is invalid");
+	      return;
+	    }
+	    int[][] signUpsLookup = makeSignUpLookup(numSteps, signUps);
+	    int[][] schedule = scheduleExperiments(numStudents, numSteps, signUpsLookup);
+	    printResults(schedule);
+	    System.out.println("");
+	  }
+	
+	  
+	  public static void main(String args[]){
+		  PhysicsExperiment pe = new PhysicsExperiment();
+	
+
+	    // Experiment 1: Example 1 from README, 3 students, 6 steps:
+	    int[][] signUpsExperiment1 = {{1, 2, 3, 5}, {2, 3, 4}, {1, 4, 5, 6}};
+	    pe.makeExperimentAndSchedule(1, 3, 6, signUpsExperiment1);
+	
+
+	    // Experiment 2: Example 2 from README, 4 students, 8 steps
+	    int[][] signUpsExperiment2 = {{5, 7, 8}, {2, 3, 4, 5, 6}, {1, 5, 7, 8}, {1, 3, 4, 8}};
+	    pe.makeExperimentAndSchedule(2, 4, 8, signUpsExperiment2);
+	
+
+	    // Experiment 3: Another test case, 5 students, 11 steps
+	    int[][] signUpsExperiment3 = {{7, 10, 11}, {8, 9, 10}, {2, 3, 4, 5, 7}, {1, 5, 6, 7, 8}, {1, 3, 4, 8}};
+	    pe.makeExperimentAndSchedule(3, 5, 11, signUpsExperiment3);
+	  }
 }
